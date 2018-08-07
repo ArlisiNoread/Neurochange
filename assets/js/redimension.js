7dea1ed714ee76f0fb3a62db31e44dba2b23dc9b
ArlisiNoread <41275777+ArlisiNoread@users.noreply.github.com>
2018-08-06 23:37:39 -0500

    Slider con Texto

--- assets/js/redimension.js
+++ assets/js/redimension.js
@@ -3,12 +3,12 @@ var navbarHeight = $('#barraNavegacion').outerHeight(true);
 $(document).ready(function(){
 
     console.log('Redimensión al cargar ' + $(window).height());
-    $('#fondo-portada').css('height', $(window).height() - navbarHeight);
+    $('div.jumbotron.carousel-hero').css('height', $(window).height() - navbarHeight);
 });
 
 $(window).resize(function(){ 
 
     console.log('Redimensión al cambiar ventana ' + $(window).height());
-    $('#fondo-portada').css('height', $(window).height() - navbarHeight);
+    $('div.jumbotron.carousel-hero').css('height', $(window).height() - navbarHeight);
 });
 
