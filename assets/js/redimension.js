7002ac9b1a4cbe44caf1eba210bd20a153cdfbe8
ArlisiNoread <41275777+ArlisiNoread@users.noreply.github.com>
2018-08-07 21:52:29 -0500

    Commit cambios en sección 1

--- assets/js/redimension.js
+++ assets/js/redimension.js
@@ -3,12 +3,12 @@ var navbarHeight = $('#barraNavegacion').outerHeight(true);
 $(document).ready(function(){
 
     console.log('Redimensión al cargar ' + $(window).height());
-    $('div.jumbotron.carousel-hero').css('height', $(window).height() - navbarHeight);
+    $('#main-imagen').css('height', $(window).height() - navbarHeight);
 });
 
 $(window).resize(function(){ 
 
     console.log('Redimensión al cambiar ventana ' + $(window).height());
-    $('div.jumbotron.carousel-hero').css('height', $(window).height() - navbarHeight);
+    $('#main-imagen').css('height', $(window).height() - navbarHeight);
 });
 
