f9569598c54c08c7c601734c1fe328b9cf9e1aed
ArlisiNoread <41275777+ArlisiNoread@users.noreply.github.com>
2018-08-08 01:51:36 -0500

    Más información Margoth

--- assets/js/efectos.js
+++ assets/js/efectos.js
@@ -24,32 +24,32 @@ $(".animation-tracking-in-expand").inViewport(function(px){
 
 /* Efectos de boton Mostrar Más */
 
-$(document).ready(function() {
-    $("#masSobreMargoth").click(function(){
-        var valor = $(".oculta-seccion").css("visibility");
-        if(valor.localeCompare("hidden") == 0){
-            $('.oculta-seccion').css({
-                'visibility' : 'visible', 
-                'opacity' : '1', 
-                '-webkit-transition' : 'visibility 0s linear 0s, opacity 600ms', /* Safari */
-                'transition' : 'visibility 0s linear 0s, opacity 600ms',
-                'height' : 'auto',
-            });
-
-        }
-        else{
-            $('.oculta-seccion').css({
-                'visibility' : 'hidden', 
-                'opacity' : '0', 
-                '-webkit-transition' : 'visibility 0s linear 300ms, opacity 300ms', /* Safari */
-                'transition' : 'visibility 0s linear 300ms, opacity 300ms',
-                'height' : '0',
-                'overflow' : 'hidden'
-            });
-     
-        }
-    }); 
-});
+//$(document).ready(function() {
+//    $("#masSobreMargoth").click(function(){
+//        var valor = $(".oculta-seccion").css("visibility");
+//        if(valor.localeCompare("hidden") == 0){
+//            $('.oculta-seccion').css({
+//                'visibility' : 'visible', 
+//                'opacity' : '1', 
+//                '-webkit-transition' : 'visibility 0s linear 0s, opacity 600ms', /* Safari */
+//                'transition' : 'visibility 0s linear 0s, opacity 600ms',
+//                'height' : 'auto',
+//            });
+//
+//        }
+//        else{
+//            $('.oculta-seccion').css({
+//                'visibility' : 'hidden', 
+//                'opacity' : '0', 
+//                '-webkit-transition' : 'visibility 0s linear 300ms, opacity 300ms', /* Safari */
+//                'transition' : 'visibility 0s linear 300ms, opacity 300ms',
+//                'height' : '0',
+//                'overflow' : 'hidden'
+//            });
+//     
+//        }
+//    }); 
+//});
 
 
 
--- assets/js/masInformacionFlotante.js
+++ assets/js/masInformacionFlotante.js
@@ -1,3 +1,5 @@
+/* Botón más info */
+
 $(document).ready(function() {
     $("#boton-abierto-masInformacion-flotante").click(function(){
             $('#masInformacion-flotante').css({
@@ -23,3 +25,32 @@ $(document).ready(function() {
         
     }); 
 });
+
+/* Botón info Margoth */
+
+$(document).ready(function() {
+    $("#masSobreMargoth").click(function(){
+            $('#masInformacion-flotante-2').css({
+                'z-index' : '550',
+                'visibility' : 'visible', 
+                'opacity' : '1', 
+                '-webkit-transition' : 'visibility 0s linear 0s, opacity 600ms', /* Safari */
+                'transition' : 'visibility 0s linear 0s, opacity 600ms'
+            });
+        
+    }); 
+    
+    
+    $("#boton-cerrado-masInformacion-flotante-2").click(function(){
+
+            $('#masInformacion-flotante-2').css({
+                'z-index' : '-550',
+                'visibility' : 'hidden', 
+                'opacity' : '0', 
+                '-webkit-transition' : 'visibility 0s linear 300ms, opacity 300ms', /* Safari */
+                'transition' : 'visibility 0s linear 300ms, opacity 300ms'
+            });
+        
+    }); 
+});
+
